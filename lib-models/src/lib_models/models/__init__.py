323033429738b1aa77551d22fd89644d8fb5479b
Russell Johnson <russjohnson09@gmail.com>
2025-11-14 01:45:57 -0500

    parquet load (#3)
    * parquet load
    
    * more pandas tests
    
    * user session tests
    
    * lib models
    
    * added lib model example
    
    * removed

---
+++ lib-models/src/lib_models/models/__init__.py
@@ -0,0 +1,2 @@
+from .user import User
+from .user2 import User2
---
+++ lib-models/src/lib_models/models/user2.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from lib_models import Base
+
+
+# 3. Define a table as a Python class
+class User2(Base):
+    __tablename__ = 'users2'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}', email='{self.email}')>"
