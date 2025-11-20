1b2156a9ec419717ff0dc7e032bcc2dca5eda819
Russell Johnson <russjohnson09@gmail.com>
2025-11-20 01:15:40 -0500

    update

--- lib-sounds/src/lib_sounds/__init__.py
+++ lib-sounds/src/lib_sounds/__init__.py
@@ -45,3 +45,90 @@ class Piano():
 
         return piano_note.play()
 
+
+
+# TODO defaults here should be a shared default which is also passed to the pygame.mixer
+# or pull the current values from pygame.mixer??
+def _load_sound_segment(snd: pygame.mixer.Sound | bytearray,
+                        start_seconds,
+                        end_seconds,
+        frequency: int = 44100,
+
+        # The size argument represents how many bits are used for each audio sample. If the value is negative then signed sample values will be used. Positive values mean unsigned audio samples will be used. An invalid value raises an exception.
+        size: int = -16,
+        channels: int = 2
+                        ):
+    
+    bytes_per_second = int(frequency * channels * abs(size / 8))
+    # start_seconds = 5.282
+    # end_seconds = 5.955
+
+    start_byte = int(bytes_per_second * start_seconds)
+    end_byte = int(bytes_per_second * end_seconds)
+
+    if isinstance(snd, pygame.mixer.SoundType):
+        raw: bytes = snd.get_raw()
+        byte_array =  bytearray(raw)
+    else:
+        byte_array = snd
+
+
+
+
+    return pygame.mixer.Sound(byte_array[start_byte:end_byte])
+
+
+class AndSoundBoard():
+    def __init__(self):
+        filename = os.path.join(ASSET_DIR, f'&.mp3')
+        # super().__init__(filename)
+        sound = pygame.mixer.Sound(filename)
+        raw: bytes = sound.get_raw()
+        byte_array =  bytearray(raw)
+        self.byte_array = byte_array
+        self.sound = _load_sound_segment(sound,
+                        start_seconds =  5.282,
+                        end_seconds =  5.955,
+                                         )
+
+    # https://stackoverflow.com/questions/68756636/how-to-play-an-audio-file-starting-at-a-specific-time-in-pygame
+    def play(self, name):
+        start_seconds =  0
+        end_seconds =  None
+        if name == 'came':
+            start_seconds =  5.282
+            end_seconds =  5.955
+        
+        if end_seconds is None:
+            return
+        snd = _load_sound_segment(self.byte_array, start_seconds, end_seconds)
+        snd.play()
+
+# mixer.music.play(start=1.3)
+# Came
+
+# I probably want the file not being there to be handled gracefully.
+# Log an error so that I can correct missing sounds but music is going to be some of the bigger assets.
+# https://www.pygame.org/docs/ref/music.html#pygame.mixer.music.play
+# https://www.pygame.org/docs/ref/mixer.html?highlight=sound#pygame.mixer.Sound
+# Sound(buffer=buffer) -> Sound
+# I can probably clip the start and end as a buffer?
+# pygame.mixer.music.set_pos()
+
+# https://www.pygame.org/docs/ref/mixer.html#pygame.mixer.Sound
+class Came():
+    def __init__(self):
+        filename = os.path.join(ASSET_DIR, f'&.mp3')
+        # super().__init__(filename)
+        sound = pygame.mixer.Sound(filename)
+        self.sound = _load_sound_segment(sound,
+                        start_seconds =  5.282,
+                        end_seconds =  5.955,
+                                         )
+
+    # https://stackoverflow.com/questions/68756636/how-to-play-an-audio-file-starting-at-a-specific-time-in-pygame
+    def play(self):
+        self.sound.play()
+        # pygame.mixer.music.play()
+        # print(self.sound.get_raw())
+        # super().play(loops, start, fade_ms)
