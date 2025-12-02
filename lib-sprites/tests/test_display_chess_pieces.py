5cb834b549bf97846a15781a1d78c25f4f03c266
Russell Johnson <russjohnson09@gmail.com>
2025-12-01 23:49:00 -0500

    update

--- lib-sprites/tests/test_display_chess_pieces.py
+++ lib-sprites/tests/test_display_chess_pieces.py
@@ -1,6 +1,6 @@
 
 
-from lib_sprites import ShipSprite, BeeSprite, ShrimpSprite, ShipExplosion, WhitePawn
+from lib_sprites import ShipSprite, BeeSprite, ShrimpSprite, ShipExplosion, WhitePawn, ChessBoard
 import pygame
 
 from .conftest import get_screen
@@ -18,12 +18,39 @@ def test_main():
 
     clock = pygame.time.Clock()
 
+    bg_sprites =  pygame.sprite.Group()
+    board = ChessBoard()
+
+    bg_sprites.add(board)
+
     player_sprites =  pygame.sprite.Group()
 
-    pawn1 = WhitePawn()
-    pawn1.rect.x = 18
-    pawn1.rect.y = 32
-    player_sprites.add(pawn1)
+    # pawn1 = WhitePawn()
+    # pawn1.rect.x = 65
+    # pawn1.rect.y = 34
+
+    # player_sprites.add(pawn1)
+
+    # pawn2 = WhitePawn()
+    # pawn2.rect.x = 65 + 50
+    # pawn2.rect.y = 34 + 0
+    
+    # player_sprites.add(pawn2)
+
+
+    # pawn3 = WhitePawn()
+    # pawn3.rect.x = 65 + 50 * 2
+    # pawn3.rect.y = 34 + 0
+    
+    # player_sprites.add(pawn3)
+
+    for row_idx in range(0,8):
+        for col_idx in range(0,8):
+            pawn3 = WhitePawn()
+            pawn3.rect.x = 66 + 48 * col_idx
+            pawn3.rect.y = 34 + 48 * row_idx
+            
+            player_sprites.add(pawn3)
 
 
 
@@ -31,6 +58,9 @@ def test_main():
     while i < (60 * 10):
         pygame.event.get()
         screen.fill(FILL)
+
+        bg_sprites.update()
+        bg_sprites.draw(screen)
 
         player_sprites.update()
         player_sprites.draw(screen)
