80ea46e980cec0e56d289c31ce6604f7c4740de8
unknown <russjohnson09@gmail.com>
2025-12-06 22:03:26 -0500

    tcp server

--- lib-network/src/lib_network/tcp_server.py
+++ lib-network/src/lib_network/tcp_server.py
@@ -24,7 +24,7 @@ class TcpServer(
     # uv run client.py --key=123121321
     # verify the hashed password.
     # I'd like to also verify the client cert if possible at some point.
-    def __init__(self, cert_dir = None):
+    def __init__(self, cert_dir = None, host = '0.0.0.0'):
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         # TODO at some point enable this?
         # context.verify_mode = ssl.CERT_REQUIRED
@@ -42,26 +42,34 @@ class TcpServer(
         context.load_cert_chain(cert, key)
 
         self._context = context
-
-
+        self._host = host
+        self._port = 9999
+        self._buffer_size = 1024
                 
 
 
         pass
 
-    def receive(port: int = 9999, buffer_size: int = 1024):
+    def start(self):
+        print("start")
+        host = self._host
+        port = self._port
+        buffer_size = self._buffer_size
 
+        print("receive", self._host, self._port)
 
-
-        host = ""
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("created new socket")
         # Address might be in a TIME_WAIT status, ignore this
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        print("bind", (host, port))
         sock.bind((host, port))
         sock.listen()
+        print("socket is listening", (host, port))
+        sock = self._context.wrap_socket(sock, server_side=True)
 
-        sock = context.wrap_socket(sock, server_side=True)
-
+        print("start accepting connections")
         conn, addr = sock.accept()
         while True:
             data = conn.recv(buffer_size)
--- lib-network/tests/test_server.py
+++ lib-network/tests/test_server.py
@@ -1,6 +1,11 @@
 from lib_network import TcpServer
+from time import sleep
+import threading
 
-
+def _start_tcp_thread(tcp_server):
+    print("_start_tcp_thread")
+    print(tcp_server)
+    tcp_server.start()
 
 def test_server():
 
@@ -9,3 +14,11 @@ def test_server():
     assert 1 == 1
 
     tcp_server = TcpServer()
+
+    threading.Thread(
+        target=_start_tcp_thread,
+        args=(tcp_server,)
+        # target=tcp_server.receive
+        ,daemon=True).start()
+
+    sleep(2)
