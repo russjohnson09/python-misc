77c66aab09ecb61be6820f58c4b6901ab4205414
Russell Johnson <russjohnson09@gmail.com>
2025-12-14 17:46:04 -0500

    saved libsum.so

--- libs/c/hello_world/main.py
+++ libs/c/hello_world/main.py
@@ -1,3 +1,16 @@
+import ctypes
+import os
+
+
+
+_root_repo_dir = os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../../'))
+# full path from os
+_sum = ctypes.CDLL('./libsum.so')
+# _sum.our_function.argtypes = (ctypes.c_int, ctypes.POINTER(ctypes.c_int))
+_default_lib_dir = os.path.abspath(os.path.join(_root_repo_dir, 'assets'))
+
+os.environ['ASSET_DIR'] = _default_asset_dir
+
 def main():
     print("Hello from hello-world!")
 
