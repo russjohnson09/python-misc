8c6c379c161d4fe9dc24d67672e2d12528c606c8
Russell Johnson <russjohnson09@gmail.com>
2025-12-14 21:13:19 -0500

    success

---
+++ libs/lib-bulletchess/src/main.c
@@ -0,0 +1,22 @@
+// #include <pybind11/pybind11.h>
+// #include <Python.h>
+#include "internal/board.h"
+
+#include "internal/fen.h"
+
+
+
+
+// #include "bulletchess.c"
+
+// std::string hello_from_bin() { return "Hello from lib-bulletchess!"; }
+
+// namespace py = pybind11;
+
+// PYBIND11_MODULE(_core, m) {
+//   m.doc() = "pybind11 hello module";
+
+//   m.def("hello_from_bin", &hello_from_bin, R"pbdoc(
+//       A function that returns a Hello string.
+//   )pbdoc");
+// }
