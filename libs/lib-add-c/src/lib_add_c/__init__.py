d37d2d9574fc7c403d6d9efcd40ed556f4784cb9
Russell Johnson <russjohnson09@gmail.com>
2025-12-14 20:14:36 -0500

    test lib add

--- libs/lib-add-c/src/lib_add_c/__init__.py
+++ libs/lib-add-c/src/lib_add_c/__init__.py
@@ -1,6 +1,9 @@
-from lib_add_c._core import hello_from_bin
+from lib_add_c._core import hello_from_bin, add
 
-# maybe I need to import add for it to be exposed?
+# # maybe I need to import add for it to be exposed?
 
-def main() -> None:
-    print(hello_from_bin())
+# def main() -> None:
+#     print(hello_from_bin())
+
+
+__all__ = [hello_from_bin, add]
