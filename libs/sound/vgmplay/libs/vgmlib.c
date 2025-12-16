a02d825b7829c5abc32bba47f840adef833108c8
russjohnson09@gmail.com <russjohnson09@gmail.com>
2025-12-15 21:21:18 -0500

    build more chips

--- libs/sound/vgmplay/libs/vgmlib.c
+++ libs/sound/vgmplay/libs/vgmlib.c
@@ -2,6 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// https://www.geeksforgeeks.org/dsa/sin-in-c/
+#include <stdio.h>
+
+// /usr/bin/ld: libvgm.so: undefined reference to `sin'
+// /usr/bin/ld: libvgm.so: undefined reference to `pow'
+#include <math.h>
+
+
+// /usr/bin/ld: libvgm.so: undefined reference to `DUMMYBUF'
+#include "VGMPlay/chips/mamedef.h"
+
+
+// create extern stream_sample_t* DUMMYBUF[];
+// This is a global stream used for all chips
+stream_sample_t* DUMMYBUF[0x02] = {NULL, NULL};
+
+// libs\sound\vgmplay\libs\VGMPlay\chips\adlibemu_opl3.c
+// #include "VGMPlay/chips/adlibemu_opl3.c"
+
+// another extern where is the h file for this one??
+// libs\sound\vgmplay\libs\VGMPlay\VGMPlay.c
+INT32 CHIP_SAMPLE_RATE;
+UINT8 CHIP_SAMPLING_MODE;
+
+
+// /usr/bin/ld: libvgm.so: undefined reference to `adlib_OPL3_reg_read'
+//         run: sudo apt-get install make gcc zlib1g-dev libao-dev libdbus-1-dev  -y
+
+// #define ADLIBEMU(name)			adlib_OPL3_##name
+// libs\sound\vgmplay\libs\VGMPlay\chips\adlibemu.h
+
+
+// DUMMYBUF
+
+// libs\sound\vgmplay\libs\VGMPlay\VGMPlay.c
+// extern stream_sample_t* DUMMYBUF[];
 
 
 int hello_world() {
