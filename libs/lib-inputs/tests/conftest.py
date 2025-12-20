1f781eaeb26505c65b6c08b8e77a9d99c1729d40
Russell Johnson <russjohnson09@gmail.com>
2025-12-20 12:15:33 -0500

    feat: init libs-input (#46)

---
+++ libs/lib-inputs/tests/conftest.py
@@ -0,0 +1,73 @@
+
+import pygame
+import os
+from lib_inputs import InputHandler
+
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+FILL = (5, 5, 5)
+
+class TestHandler():
+    screen = None
+    is_ci_test = False
+    is_pygame_init = False
+    
+    def __init__(self, is_ci_test = os.environ.get('IS_CI_TEST', '0') == '1'):
+        self.is_ci_test = is_ci_test
+        pass
+
+    def init_pygame(self):
+
+        if not self.is_pygame_init:
+            print("pygame init")
+            pygame.init()
+            # pygame.mixer.init()
+            # int fonts for display
+            self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE | pygame.SCALED)
+        else:
+            print("pygame init already called. to change screensize call ...")
+        
+        self.is_pygame_init = True
+        self.font = pygame.font.SysFont("Arial" , 18 , bold = False)
+        self.clock = pygame.time.Clock()
+
+
+
+
+        pass
+
+    def do_iteration(self, ih: InputHandler, callback):
+
+        for event in pygame.event.get():
+            ih.handle_event(event)
+            if event.type == pygame.QUIT:
+                return False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+        
+        self.screen.fill(FILL)
+
+        callback()
+
+        pygame.display.flip()
+
+        self.clock.tick(60)
+        return True
+    pass
+
+test_handler = TestHandler()
+
+# https://stackoverflow.com/questions/34466027/what-is-conftest-py-for-in-pytest
+
+# for some reason it seems like this wasn't being called.
+# not calling pygame.init can lead to a segmentation fault that is hard to debug
+
+# called between each test
+def pytest_runtest_setup():
+    print("")
+    print("pytest_runtest_setup")
+    test_handler.init_pygame()
+
---
+++ libs/lib-inputs/tests/test_input_handler.py
@@ -0,0 +1,179 @@
+from lib_inputs import InputHandler
+from .conftest import test_handler
+import pygame
+
+from unittest.mock import MagicMock
+
+
+def _write_instructions_text(instructions, ih):
+    instruction_text = test_handler.font.render(instructions, 1, pygame.Color("RED"))
+
+    pos_y = 0
+    test_handler.screen.blit(instruction_text,(0,0))
+
+    pos_y += 20
+    test_handler.screen.blit(
+        test_handler.font.render(f"Mouse pos: {str(ih.get_mouse_pos())}", 1, pygame.Color("RED"))
+        ,(0,pos_y))
+    
+    pos_y += 20
+    test_handler.screen.blit(
+        test_handler.font.render(f"south: {str(ih.south)}", 1, pygame.Color("RED"))
+        ,(0,pos_y))
+    
+    pos_y += 20
+    test_handler.screen.blit(
+        test_handler.font.render(f"primary: {str(ih.primary)}", 1, pygame.Color("RED"))
+        ,(0,pos_y))
+    
+    pos_y += 20
+    test_handler.screen.blit(
+        test_handler.font.render(f"west: {str(ih.west)}", 1, pygame.Color("RED"))
+        ,(0,pos_y))
+
+    pass
+
+def _mock_mouse_input_event(ih, button, down = True):
+    event = MagicMock()
+    event.button = button
+    if down:
+        event.type = pygame.MOUSEBUTTONDOWN
+    else:
+        event.type = pygame.MOUSEBUTTONUP
+
+    print("mock input", event.type, event.button)
+    ih.handle_event(event)
+
+
+# lib-sprites\tests\test_mouse.py
+def test_mouse_primary_click():
+
+    event = MagicMock()
+    event.type = pygame.MOUSEBUTTONDOWN
+    event.button = 1
+
+    print(event)
+    print(event.type)
+    print(event.button)
+
+    ih = InputHandler(pygame)
+
+    mouse_pos = (0,0)
+
+    instructions = "Update"
+    def callback():
+        _write_instructions_text(instructions, ih)
+        return
+
+    # feed the input handler events.
+    # It also should have access to things like fetching the mouse position.
+
+    is_first_loop = True
+    while(test_handler.do_iteration(ih, callback)):
+        instructions = "Left Click Press And Hold"
+        mouse_pos = ih.get_mouse_pos()
+        assert mouse_pos is not None
+        assert len(mouse_pos) == 2
+
+        if is_first_loop:
+            # fire event is being pressed
+            assert ih.south is False
+            assert ih.primary is False
+
+        if test_handler.is_ci_test:
+            _mock_mouse_input_event(ih, 1)
+
+        if ih.south is True:
+            break
+        # default bindings
+        is_first_loop = False
+        pass
+    is_first_loop = True
+
+    while(test_handler.do_iteration(ih, callback)):
+        instructions = "Left Click Release"
+
+        mouse_pos = ih.get_mouse_pos()
+        assert mouse_pos is not None
+        assert len(mouse_pos) == 2
+
+        if is_first_loop:
+            # fire event is being pressed
+            assert ih.south is True
+            assert ih.primary is True
+
+        if test_handler.is_ci_test:
+            _mock_mouse_input_event(ih, 1, False)
+
+        if ih.south is False:
+            break
+        # default bindings
+        is_first_loop = False
+        pass
+
+    assert ih.south is False
+    assert ih.primary is False
+
+
+    pass
+
+
+def test_mouse_right_click():
+
+    ih = InputHandler(pygame)
+
+    mouse_pos = (0,0)
+
+    instructions = "Update"
+    def callback():
+        _write_instructions_text(instructions, ih)
+        return
+
+    # feed the input handler events.
+    # It also should have access to things like fetching the mouse position.
+
+    is_first_loop = True
+    while(test_handler.do_iteration(ih, callback)):
+        instructions = "Right Click Press And Hold"
+        mouse_pos = ih.get_mouse_pos()
+        assert mouse_pos is not None
+        assert len(mouse_pos) == 2
+
+        if is_first_loop:
+            # fire event is being pressed
+            assert ih.west is False
+
+        if test_handler.is_ci_test:
+            _mock_mouse_input_event(ih, 3)
+
+        if ih.west is True:
+            break
+        # default bindings
+        is_first_loop = False
+        pass
+    is_first_loop = True
+
+    while(test_handler.do_iteration(ih, callback)):
+        instructions = "Right Click Release"
+
+        mouse_pos = ih.get_mouse_pos()
+        assert mouse_pos is not None
+        assert len(mouse_pos) == 2
+
+        if is_first_loop:
+            # fire event is being pressed
+            assert ih.west is True
+
+        if test_handler.is_ci_test:
+            _mock_mouse_input_event(ih, 3, False)
+
+        if ih.west is False:
+            break
+        # default bindings
+        is_first_loop = False
+        pass
+
+    assert ih.west is False
+
+
+    pass
